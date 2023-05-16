6c3b2d80420d1ae86bc499f50a308cf81b049c82
Darwin Becker <darwinbecker@web.de>
2023-05-17 01:35:59 +0200

    Blac Eyes Peas query setup

--- src/main/java/org/htw/Main.java
+++ src/main/java/org/htw/Main.java
@@ -3,6 +3,7 @@ package org.htw;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.log4j.BasicConfigurator;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,24 +11,32 @@ import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
 
-        // Open the bloggers RDF graph from the filesystem
+        // Open the Black Eyed Peas RDF graph from the filesystem
         try {
-            InputStream in = new FileInputStream(new File("bloggers.rdf"));
+            InputStream in = new FileInputStream(new File("./rdf/BlackEyedPeas-v1.rdf"));
 
             // Create an empty in‑memory model and populate it from the graph
-            Model model = ModelFactory.createMemModelMaker().createModel("");
+            Model model = ModelFactory.createMemModelMaker().createDefaultModel();
             model.read(in, null); // null base URI, since model URIs are absolute
             in.close();
 
             // Create a new query
             String queryString =
-                    "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
-                            "SELECT ?url " +
+                    "PREFIX bep: <http://www.semanticweb.org/aminesellami/ontologies/2023/4/blackEyedPeas/> " +
+                            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+
+                            "SELECT ?bandName ?albumTitle " +
                             "WHERE {" +
-                            "      ?contributor foaf:name \"Jon Foobar\" . " +
-                            "      ?contributor foaf:weblog ?url . " +
-                            "      }";
+                            "bep:BlackEyedPeas rdf:type bep:Band ; " +
+                            "bep:name ?bandName ; " +
+                            "bep:hasAlbum ?album . " +
+
+                            "?album rdf:type bep:Album ; " +
+                            "bep:name ?albumTitle . " +
+                            "}";
 
             Query query = QueryFactory.create(queryString);
 
